69a0c3ac6d4dbf9f85aeb193e494051cb4874e05
Jackson <jegerard@ucsc.edu>
2021-02-02 09:06:16 -0800

    Got it working with background sound!
    See above

--- SpeedBurner/Assets/Scripts/UITextBehavior.cs
+++ SpeedBurner/Assets/Scripts/UITextBehavior.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityOSC;
 public class UITextBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -11,7 +12,8 @@ public class UITextBehavior : MonoBehaviour
     public int speed;
     void Start()
     {
-        
+        OSCHandler.Instance.Init ();
+        OSCHandler.Instance.SendMessageToClient("pd", "/unity/trigger", "ready");
     }
 
     // Update is called once per frame
@@ -22,12 +24,16 @@ public class UITextBehavior : MonoBehaviour
         gameText.color = Color.black;
         if(speed < 10){
             scoreString = "00" + speed.ToString();
+
             gameText.color = Color.red;
+            OSCHandler.Instance.SendMessageToClient ("pd", "/unity/trigger", speed);
         }else if(speed < 100){
             scoreString = "0" + speed.ToString();
             gameText.color = Color.red;
+            OSCHandler.Instance.SendMessageToClient ("pd", "/unity/trigger", speed);
         }else{
             scoreString = speed.ToString();
+            OSCHandler.Instance.SendMessageToClient ("pd", "/unity/trigger", speed);
         }
         gameText.text = "Stock: " + scoreString + "USD"; 
         
